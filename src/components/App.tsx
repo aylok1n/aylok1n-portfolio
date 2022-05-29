fd0fce51880219e466af0d2ac32c4993bfb2da68
aylok1n <nikolya.blog@gmail.com>
2022-05-29 10:36:53 +0400

    Add skills

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ScrollObserver from '../utils/scrollObserver';
 import Header from './header';
+import Skills from './Skills';
 
 const App: React.FC = () => (
   <ScrollObserver>
     <Header />
+    <Skills />
   </ScrollObserver>
 );
 export default App;
