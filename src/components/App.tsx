1d5b600d122271e8f0c8a58f75e9e29d5ba8f5ba
aylok1n <nikolya.blog@gmail.com>
2022-05-29 09:42:16 +0400

    Add fonts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ScrollObserver from '../utils/scrollObserver';
-import AboutMe from './aboutMe';
 import Header from './header';
 
 const App: React.FC = () => (
   <ScrollObserver>
     <Header />
-    <AboutMe />
   </ScrollObserver>
 );
 export default App;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,19 +1,23 @@
-import React, { useContext, useRef } from 'react';
+import React, { useCallback, useContext, useRef, useState } from 'react';
 import { ScrollContex } from '../../utils/scrollObserver';
 import background from '../../assets/video/background.m4v';
 import backgroundWebm from '../../assets/video/background.webm';
 import IconDown from '../../assets/images/icon-arrow-down.png';
 
 const Header: React.FC = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
   const refContainer = useRef<HTMLDivElement>(null);
   const { scrollY } = useContext(ScrollContex);
 
   let progress = 0;
-
   const { current: elContainer } = refContainer;
   if (elContainer) {
     progress = Math.min(1, scrollY / elContainer.clientHeight);
   }
+
+  const handleImageLoaded = useCallback(() => {
+    setImageLoaded(true);
+  }, []);
 
   return (
     <div
@@ -45,13 +49,23 @@ const Header: React.FC = () => {
         className="flex z-10 flex-col flex-1 justify-center items-center p-12 font-bold text-center
       text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)]"
       >
-        <h1 className="mb-6 text-4xl xl:text-5xl">aylok1n</h1>
+        <h1 className="mb-6 text-4xl xl:text-5xl">AYLOK1N</h1>
         <h2 className="mb-2 text-2xl tracking-tight xl:text-3xl">
-          <span>Portfolio site</span>
+          <span>Сайт портфолио</span>
         </h2>
       </div>
-      <div className="z-10 grow-0 pb-20 transition-opacity duration-1000 md:pb-10">
-        <img src={IconDown} alt="scroll" width={128 / 3} height={128 / 3} />
+      <div
+        className={`z-10 grow-0 pb-20 transition-all duration-1000 md:pb-10 ${
+          imageLoaded ? 'opacity-100' : 'opacity-0 -translate-y-10'
+        }`}
+      >
+        <img
+          src={IconDown}
+          alt="scroll"
+          width={128 / 3}
+          height={128 / 3}
+          onLoad={handleImageLoaded}
+        />
       </div>
     </div>
   );
